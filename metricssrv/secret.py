86a988cd3b999c6a2aea42ff8d2b4a76c65f4681
Alexander Bersenev <bay@hackerdom.ru>
2021-02-27 19:09:32 +0500

    add secret file

---
+++ metricssrv/secret.py
@@ -0,0 +1,2 @@
+SECRET_COOKIE_KEY = bytes.fromhex("0000000000000000000000000000000000000000000000000000000000000000")
+DEMO_USER_PARAMS = {"user": "hackathon107", "password": "XXXXXXXXXXX", "domain": "hackathon107", "project": "ru-moscow-1"}
