9d1e0a2cdefb16d1a37129265a16f003cbf5ab7c
Pavel Vasev <pavel.vasev@gmail.com>
2021-02-27 11:28:50 +0500

    fixed pwa

--- app1/pwa/setup.js
+++ app1/pwa/setup.js
@@ -1,7 +1,7 @@
 /* Only register a service worker if it's supported */
 if ('serviceWorker' in navigator) {
   console.log("Registering sw");
-  navigator.serviceWorker.register('/apps/sber/mobin/pwa/service-worker.js', { scope: '/apps/sber/mobin/' }).then(function (registration)
+  navigator.serviceWorker.register('/app1/pwa/service-worker.js', { scope: '/app1/' }).then(function (registration)
         {
           console.log('Service worker registered successfully');
         }).catch(function (e)
